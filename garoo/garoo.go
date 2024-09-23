13f5a9430debadf30e65a84fd8525649a0d39d4c
rot1024 <aayhrot@gmail.com>
2024-09-24 03:40:57 +0900

    fix: fix logs

--- garoo/garoo.go
+++ garoo/garoo.go
@@ -238,7 +238,7 @@ func (g *Garoo) processSeed(ctx context.Context, seed Seed) error {
 
 		post.Category = seed.Category
 		post.Tags = seed.Tags
-		slog.Info("got post", "id", post.ID, "provider", post.Provider)
+		slog.Info(fmt.Sprintf("got post: %#v", seed))
 
 		for _, store := range g.stores {
 			slog.Info("saving post", "store", store.Name())
